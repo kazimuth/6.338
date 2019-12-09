75244e529c73c2ed2ade50fe761d6c1bb5a19269
James Gilles <jhgilles@mit.edu>
2019-12-08 19:05:35 -0500

    Debian...

---
+++ startup.sh
@@ -0,0 +1,7 @@
+. /data/scratch/jhgilles/miniconda3/bin/activate
+conda activate /data/scratch/jhgilles/julia
+export JUPYTER_CONFIG_DIR=/data/scratch/jhgilles/.jupyter
+#export CLOUDSDK_CONFIG=/data/scratch/jhgilles/.gcloud
+#export GOOGLE_APPLICATION_CREDENTIALS=/data/scratch/jhgilles/adv-lth/cloud/secrets/service-account-private-key.json
+#export PYTHONPATH=/data/scratch/jhgilles/adv-lth/tinylth/gpu-src:/data/scratch/jhgilles/adv-lth/tinylth/lottery:$PYTHONPATH
+export JULIA_DEPOT_PATH=/data/scratch/jhgilles/.julia
